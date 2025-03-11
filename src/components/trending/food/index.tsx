3018f1d88fa5c534118db279e9bb2767de051e7f
Julia Pereira <vital.pereira.julia@gmail.com>
2025-03-11 17:21:47 -0300

    colocando as imagens

--- src/components/trending/food/index.tsx
+++ src/components/trending/food/index.tsx
@@ -1,9 +1,12 @@
 import { Pressable, View, Text, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
 import { FoodProps } from "../";
 
 export function CardHorizontalFood({ food }: { food: FoodProps }) {
   return (
-    <Pressable>
+    <Pressable className="flex flex-col rounded-xl">
+      <Image source={{ uri: food.image }} className="w-44 h-36 rounded-xl" />
       <Text>{food.name}</Text>
     </Pressable>
   );
