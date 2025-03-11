008a0e35c215570202a21448fbe79afec8184512
Julia Pereira <vital.pereira.julia@gmail.com>
2025-03-11 14:38:50 -0300

    criando Search

---
+++ src/components/search/index.tsx
@@ -0,0 +1,11 @@
+import { View, TextInput } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+export function Search() {
+  return (
+    <View>
+      <Feather name="search" size={24} color="#64748b" />
+      <TextInput placeholder="Procure sua comida..." />
+    </View>
+  );
+}
